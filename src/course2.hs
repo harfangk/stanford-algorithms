b095284f1b05cf698c98da11ee437d5506fdb8ce
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2019-05-12 17:54:51 +0900

    Implement Dijkstra for W2 assignment
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- src/course2.hs
+++ src/course2.hs
@@ -3,6 +3,7 @@ module Course2 where
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import qualified Data.List as List
+import qualified Debug.Trace
 
 type Graph = Map.Map Int [Int]
 
@@ -14,8 +15,16 @@ sccMain = do
       g = List.foldr (\x acc -> Map.insertWith (++) (List.head x) [List.last x] acc) baseG parsedList :: Graph
       revG = List.foldr (\x acc -> Map.insertWith (++) (List.last x) [List.head x] acc) baseG parsedList :: Graph
       firstRunResult =
-        Map.foldrWithKey (\k v acc -> if Set.member k (fst acc) then acc else dfsForFinishingTime revG k acc) (Set.empty, []) revG
-      (_, resultSccs, _) = List.foldl' (\(visited, sccs, leader) x -> if Set.member x visited then (visited, sccs, leader) else dfsForScc g x (visited, sccs, x)) (Set.empty, Map.empty, 0) (snd firstRunResult)
+        Map.foldrWithKey (\k _ acc ->
+                            if Set.member k (fst acc) then
+                              acc
+                            else
+                              dfsForFinishingTime revG k acc) (Set.empty, []) revG
+      (_, resultSccs, _) = List.foldl' (\(visited, sccs, leader) x ->
+                                          if Set.member x visited then
+                                            (visited, sccs, leader)
+                                          else
+                                            dfsForScc g x (visited, sccs, x)) (Set.empty, Map.empty, 0) (snd firstRunResult)
   print . List.take 5 . List.sortBy (flip compare) . List.map (List.length . snd) . Map.toList $ resultSccs
 
 dfsForFinishingTime :: Graph -> Int -> (Set.Set Int, [Int]) -> (Set.Set Int, [Int])
@@ -50,6 +59,78 @@ dfsForScc graph source (visited, sccs, leader) =
     in
       (resultVisited, Map.insertWith (++) leader [source] resultSccs, resultLeader)
 
-addIndex :: [a] -> [(Int, a)]
-addIndex =
-  List.reverse . fst . List.foldr (\x (acc, i) -> ((i, x) : acc, i + 1 )) ([], 0)
+type WeightedGraph = Map.Map Int [(Int, Int)]
+
+dijkstraMain :: IO ()
+dijkstraMain = do
+  file <- readFile "src/dijkstraData.txt"
+  let graph =  List.foldl (\acc (v, es) -> Map.insert v es acc) Map.empty . List.map (parseAdjacencyList . words) . lines $ file :: WeightedGraph
+      minDists = Map.fromAscList (List.map (\k -> (k,1000000)) [1.. 200])
+      result = dijkstra graph (Node 0 1 []) minDists
+      targets = [7,37,59,82,99,115,133,165,188,197]
+      answer = List.map (\i -> result Map.! i) targets
+  print answer
+
+parseAdjacencyList :: [String] -> (Int, [(Int, Int)])
+parseAdjacencyList [] = (0, [])
+parseAdjacencyList (x:xs) =
+  (read x, List.map (listToTuple . List.map read . splitByComma) xs)
+  where
+    listToTuple :: [Int] -> (Int, Int)
+    listToTuple is =
+      case is of
+        [i,j] -> (i,j)
+        _ -> (0,0)
+    splitByComma :: String -> [String]
+    splitByComma s  = case List.dropWhile (== ',') s of
+                        "" -> []
+                        s' -> w : splitByComma s''
+                                      where (w, s'') =
+                                             break (== ',') s'
+
+dijkstra :: WeightedGraph -> Heap Int Int -> Map.Map Int Int -> Map.Map Int Int
+dijkstra g h minDists | Map.null g || isEmpty h = minDists
+dijkstra g h minDists =
+  case Map.lookup nodeLabel g of
+    Nothing -> dijkstra g' h' minDists
+    Just es -> dijkstra g' (updateHeap d es h') minDists'
+  where
+    (d, nodeLabel, h') = extractMin h
+    g' = Map.delete nodeLabel g
+    minDists' = Map.insertWith (\newValue oldValue -> minimum [newValue, oldValue]) nodeLabel d minDists
+
+
+updateHeap :: Int -> [(Int, Int)] -> Heap Int Int -> Heap Int Int
+updateHeap d es h =
+  merge h (mergeAll (List.map (\(edgeHead, edgeWeight) -> Node (edgeWeight + d) edgeHead []) es) )
+
+data Heap k v = Empty | Node k v [Heap k v]
+
+instance (Show k, Show v) => Show (Heap k v) where
+  show h =
+    case h of
+      Empty -> "Heap.Empty"
+      Node k v hs -> "Heap.Node(" ++ show k ++ ", " ++ show v ++ ", " ++ (show . length $ hs) ++ ")"
+
+insert :: (Ord k, Ord v) => (k, v) -> Heap k v -> Heap k v
+insert (key, val) = merge (Node key val [])
+
+merge :: (Ord k, Ord v) => Heap k v -> Heap k v -> Heap k v
+merge h Empty = h
+merge Empty h = h
+merge h1@(Node key1 value1 hs1) h2@(Node key2 value2 hs2)
+  | key1 < key2 = Node key1 value1 (h2:hs1)
+  | otherwise = Node key2 value2 (h1:hs2)
+
+mergeAll :: (Ord k, Ord v) => [Heap k v] -> Heap k v
+mergeAll [] = Empty
+mergeAll [h] = h
+mergeAll (h:h':hs) = merge (merge h h') (mergeAll hs)
+
+isEmpty :: Heap k v -> Bool
+isEmpty Empty = True
+isEmpty _ = False
+
+extractMin :: (Ord k, Ord v) => Heap k v -> (k, v, Heap k v)
+extractMin Empty = error "Heap extractMin: empty Heap"
+extractMin (Node key value hs) = (key, value, mergeAll hs)
