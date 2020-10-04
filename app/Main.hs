f4f68aaf73b26df0c1e5a7996b8a5e49a3eab2ab
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2020-10-04 17:09:34 +0900

    Implement Floyd-Warshall
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import qualified Course4.Week1
+import qualified Data.Array.IArray as IArray
 
 main :: IO ()
 main = do
-  g1File <- readFile "./src/Course4/g3.txt"
-  let g1Data@(g1Vc, _, _) = Course4.Week1.parseData g1File
-      g1Result = Course4.Week1.bellmanFord (Course4.Week1.buildAdjacencyListByHead g1Data) g1Vc 1
-  print g1Result
+  (g1Vc, _, g1Edges) <- Course4.Week1.parseData "./src/Course4/g3.txt"
+  let g1 = Course4.Week1.buildAdjacencyListByTail g1Vc g1Edges
+  let g1Result = Course4.Week1.floydWarshall2 g1 g1Vc
+  let mina = fmap (minimum . IArray.elems) g1Result
+  print mina
--- src/Course4/Week1.hs
+++ src/Course4/Week1.hs
@@ -1,28 +1,29 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TupleSections #-}
 
 module Course4.Week1 where
 
-import qualified Data.List as List
-import qualified Data.Map as Map
-import qualified Data.IntMap.Strict as IntMap
-import qualified Data.Array as Array
-import qualified Control.Monad.ST as ST
-import qualified Data.HashTable.Class as HT
-import qualified Data.HashTable.ST.Cuckoo as Cuckoo
-import qualified Data.PQueue.Prio.Min as MinHeap
+import Control.Monad
+import Data.Array (Array)
+import Data.Coerce
+import Data.Maybe
+import Data.Semigroup
+import System.IO
 
-type HashTable s k v = Cuckoo.HashTable s k v
+import qualified Data.Array.IArray as IArray
+import qualified Data.Array.ST.Safe as STArray
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.IntMap.Strict as IntMap
+import qualified Data.List as List
+import qualified Data.Set as Set
 
 main :: IO ()
 main = do
-  g1File <- readFile "./src/Course4/g3.txt"
-  {-
-  g2File <- readFile "src/Course4/g2.txt"
-  g3File <- readFile "src/Course4/g3.txt"
-  -}
-  let g1Data@(g1Vc, _, _) = parseData g1File
-      --g1Result = floydWarshall2 (buildAdjacencyList g1Data) g1Vc
-      g1Result = bellmanFord (buildAdjacencyListByHead g1Data) g1Vc 1
+  (g1Vc, _, g1Edges) <- parseData "./src/Course4/test.txt"
+  let g1 = buildAdjacencyListByTail g1Vc g1Edges
+  let g1Result = floydWarshall2 g1 1
+  print g1Result
+
   {-
   let g1Data@(g1Vc, _, _) = addExtraVertexForJohnson . parseData $ g1File
       g1Result = fmap findShortestShortestPath $ johnson (buildAdjacencyList g1Data) g1Vc
@@ -36,23 +37,33 @@ main = do
   let b = seq g1Result ()
   print b
 
-parseData :: String -> (Int, Int, [(Int, Int, Double)])
-parseData s =
-  (vertexCount, edgeCount, edges)
+inf :: Double
+inf = 1/0
+
+parseData :: FilePath -> IO (Int, Int, [(Int, Int, Double)])
+parseData fp = withFile fp ReadMode $ \h -> do
+  (vertexCount, edgeCount) <- getMetaData h
+  edges <- replicateM edgeCount (getEdge h)
+  pure (vertexCount, edgeCount, edges)
   where
-    fileLines = lines s
-    (vertexCount, edgeCount) = parseMetaData . head $ fileLines
-    edges = List.map parseEdge . tail $ fileLines
-    parseMetaData :: String -> (Int, Int)
-    parseMetaData line =
-      case List.map read . words $ line of
-        [x,y] -> (x, y)
-        _ -> error "Invalid data format"
-    parseEdge :: String -> (Int, Int, Double)
-    parseEdge line =
-      case words line of
-        (edgeTail:edgeHead:weight:_) -> (read edgeTail, read edgeHead, read weight)
-        _ -> error "Invalid data format"
+    getMetaData h = parseMetaData <$> BS.hGetLine h
+    parseMetaData line = fromJust $ do
+      (x, line') <- BS.readInt line
+      (y, line'') <- BS.readInt (dropSpace line')
+      if BS.null (dropSpace line'')
+        then pure (x, y)
+        else error "Invalid data format"
+
+    getEdge h = parseEdge <$> BS.hGetLine h
+    parseEdge line = fromJust $ do
+      (edgeTail, line') <- BS.readInt line
+      (edgeHead, line'') <- BS.readInt (dropSpace line')
+      (weight, line''') <- BS.readInteger (dropSpace line'')
+      if BS.null (dropSpace line''')
+        then pure (edgeTail, edgeHead, fromInteger weight)
+        else error "Invalid data format"
+
+    dropSpace = BS.dropWhile (== ' ')
 
 addExtraVertexForJohnson :: (Int, Int, [(Int, Int, Int)]) -> (Int, Int, [(Int, Int, Int)])
 addExtraVertexForJohnson (vertexCount, edgeCount, edges) =
@@ -63,85 +74,139 @@ addExtraVertexForJohnson (vertexCount, edgeCount, edges) =
     newEdges = List.map (0,, 0) [1..vertexCount]
     edges' = edges ++ newEdges
 
-buildAdjacencyList :: (Fractional a, Ord a) => (Int, Int, [(Int, Int, a)]) -> Array.Array Int [(Int, a)]
-buildAdjacencyList (vertexCount, _, edges) =
-  Array.array (1,vertexCount) $ IntMap.toList . List.foldl' (\acc (t, h, w) -> IntMap.update (\edges' -> Just ((h,w):edges')) t acc) initialMap $ edges
+buildAdjacencyListByTail :: (Fractional a, Ord a) => Int -> [(Int, Int, a)] -> Array Int [(Int, a)]
+buildAdjacencyListByTail vC =
+  IArray.listArray (1, vC)
+  . IntMap.elems
+  . List.foldl' (\acc (t, h, w) -> IntMap.adjust ((h, w) :) t acc) initialMap
   where
-    initialMap = IntMap.fromList (List.zip [1..vertexCount] (List.repeat []))
+    initialMap = IntMap.fromAscList (List.zip [1 .. vC] (List.repeat []))
 
-buildAdjacencyListByHead :: (Fractional a, Ord a) => (Int, Int, [(Int, Int, a)]) -> Array.Array Int [(Int, a)]
-buildAdjacencyListByHead (vertexCount, _, edges) =
-  Array.array (1,vertexCount) $ IntMap.toList . List.foldl' (\acc (t, h, w) -> IntMap.update (\edges' -> Just ((t,w):edges')) h acc) initialMap $ edges
+buildAdjacencyListByHead :: (Fractional a, Ord a) => Int -> [(Int, Int, a)] -> Array Int [(Int, a)]
+buildAdjacencyListByHead vC =
+  IArray.listArray (1, vC)
+  . IntMap.elems
+  . List.foldl' (\acc (t, h, w) -> IntMap.adjust ((t, w) :) h acc) initialMap
   where
-    initialMap = IntMap.fromList (List.zip [1..vertexCount] (List.repeat []))
+    initialMap = IntMap.fromAscList (List.zip [1 .. vC] (List.repeat []))
 
-dijkstra :: IntMap.IntMap [(Int, Double)] -> Int -> Int -> Either String (IntMap.IntMap Double)
-dijkstra g vertexCount s =
-  if List.any ((>) 0 . snd) . List.concat . IntMap.elems $ g then
-    Left "Dijkstra's algorithm halted: negative edge found"
-  else
-    Right $ dijkstra' g initialHeap initialDists
-  where
-    initialDists = IntMap.fromList [generator v | v <- [1..vertexCount]]
-    initialHeap = MinHeap.insert 0 s MinHeap.empty
-    generator v
-      | v == s = (v,0)
-      | otherwise = (v,1/0)
+dijkstra :: (STArray.Ix v, Num w, Ord w, Bounded w, Fractional w) => Array v [(v, w)] -> v -> Either String (Array v w)
+dijkstra g s =
+    if List.any ((>) 0 . snd) . List.concat . IArray.elems $ g then
+      Left "Dijkstra's algorithm halted: negative edge found"
+    else
+      Right $ STArray.runSTArray $ do
+        dists <- STArray.newArray (IArray.bounds g) (1/0)
+        STArray.writeArray dists s 0
+        let dijkstra'' queue =
+              case Set.minView queue of
+                Nothing -> return ()
+                Just ((distU, u), queue') ->
+                  let edges = g IArray.! u
+                      f q (v, weight) = do
+                        let distV' = distU + weight
+                        distV <- STArray.readArray dists v
+                        if distV <= distV' then
+                          return q
+                        else do
+                          let q' = Set.delete (distV, v) q
+                          STArray.writeArray dists v distV'
+                          return $ Set.insert (distV', v) q'
+                  in
+                    foldM f queue' edges >>= dijkstra''
+        dijkstra'' (Set.singleton (0, s))
+        return dists
 
-dijkstra' :: IntMap.IntMap [(Int, Double)] -> MinHeap.MinPQueue Double Int -> IntMap.IntMap Double -> IntMap.IntMap Double
-dijkstra' _ h minDists | MinHeap.null h = minDists
-dijkstra' g h minDists =
-  dijkstra' g h'' minDists'
-  where
-    ((distU, u), h') = MinHeap.deleteFindMin h
-    adjacentVertices = List.map fst $ (IntMap.!) g u
-    (h'', minDists') = List.foldl step (h', minDists) adjacentVertices
-    step (heap, dists) v =
-      if distV > distV' then
-        (MinHeap.insert distV' v heap, IntMap.insert v distV' dists)
-      else
-        (heap, dists)
-      where
-        distV = (IntMap.!) dists v
-        weightUv = snd . head . List.filter (\(edgeHead,_) -> edgeHead == v) $ (IntMap.!) g u
-        distV' = distU + weightUv
-
-bellmanFord :: Array.Array Int [(Int, Double)] -> Int -> Int -> Either String (Array.Array Int Double)
-bellmanFord g vertexCount s =
+bellmanFord :: Array Int [(Int, Double)] -> Int -> Int -> Either String (Array Int Double)
+bellmanFord g vC s =
   if hasNegativeCycle then
     Left "Bellman-Ford algorithm halted: negative cycle found"
   else
     Right resultArray
   where
-    memo = Array.array ((0,1), (vertexCount - 1, vertexCount)) [generator (x,y) | x <- [0..(vertexCount - 1)], y <- [1..vertexCount]]
-    generator pair@(i,v) =
-      if i == 0 then
-        if v == s then
-          (pair, 0)
-        else
-          (pair, 1/0)
-      else
-        (pair, findMin pair)
-    findMin (i,v) = min ({-# SCC accessMemo #-} (Array.!) memo (i-1,v)) ({-# SCC foldl' #-} List.foldl' (\acc (t,w) -> {-# SCC foldStep #-} min acc ((Array.!) memo (i-1,t) + w)) (1/0) ({-# SCC accessGraph #-} (Array.!) g v))
-    resultList = map (\((_,v), d) -> (v,d)) . filter (\((i,_), _) -> i == vertexCount - 1 ) . Array.assocs $ memo
-    resultArray = Array.array (1, vertexCount) resultList
-    hasNegativeCycle = any (\(v, d) -> any (\(t, w) -> d > resultArray Array.! t + w) (g Array.! v)) resultList
---    hasNegativeCycle = or . concatMap (\(v,d) -> map (\(t,w) -> d > (Array.!) resultArray t + w) ((IntMap.!) g v)) $ resultList
+    calculated = STArray.runSTUArray $ do
+      array <- STArray.newArray (1, vC) inf
+      STArray.writeArray array s 0
+      relaxEdge array 1 1
+      where
+        relaxEdge array edgeCount v
+          | v > vC =
+            if edgeCount < vC
+            then relaxEdge array (edgeCount + 1) 1
+            else pure array
+          | otherwise = do
+            forM_ (g IArray.! v) $ coreCmp array v
+            relaxEdge array edgeCount (v + 1)
 
-  {-
-bellmanFord' :: IntMap.IntMap [(Int, Double)] -> Int -> Int -> Array.Array (Int,Int) Double
-bellmanFord' g vertexCount s = MArray.runSTArray $ do
-  memo <- MArray.newArray ((0,1), (vertexCount - 1, vertexCount)) (1/0)
-  MArray.writeArray memo (0,s) 0
-  CM.mapM_ (\pair -> MArray.writeArray memo pair (findMin pair memo)) [(i,v) | i <- [1..(vertexCount - 1)], v <- [1..vertexCount]]
-  return memo
+        coreCmp array v (f, w) = do
+          vW <- STArray.readArray array v
+          fW <- STArray.readArray array f
+          STArray.writeArray array v (min (fW + w) vW)
+        {-# INLINE coreCmp #-}
+
+    resultList = IArray.elems calculated
+    resultArray = IArray.listArray (1, vC) resultList
+
+    getResultOf = (resultArray IArray.!)
+    {-# INLINE getResultOf #-}
+
+    hasNegativeCycle =
+      coerce
+      . foldMap (\(t, efs) -> foldMap (\(f, w) -> coerce (getResultOf f + w < getResultOf t) :: Any) efs)
+      $ IArray.assocs g
+
+floydWarshall2 :: Array Int [(Int, Double)] -> Int -> Either String (Array (Int,Int) Double)
+floydWarshall2 g vC =
+  if hasNegativeCycle then
+    Left "Floyd-Warshall algorithm halted: negative cycle found"
+  else
+    Right resultArray
   where
-    findMin (i,v) memo =
-      minimum ((Array.!) memo (i-1,v):map (\(t,w) -> (Array.!) memo (i-1,t) + w) ((IntMap.!) g v))
-    resultList memo = map (\((_,v), d) -> (v,d)) . filter (\((i,_), _) -> i == vertexCount - 1 ) . Array.assocs $ memo
-    resultArray memo = Array.array (1, vertexCount) (resultList memo)
--}
-floydWarshall2 :: IntMap.IntMap [(Int, Double)] -> Int -> Either String (Array.Array (Int,Int) Double)
+    calculated = STArray.runSTUArray $ do
+      arr <- STArray.newArray ((1,1,0), (vC, vC, vC)) 0
+      let ixs = [(i,j) | i <- [1..vC], j <- [1..vC]]
+      forM_ ixs $ \(i,j) ->
+          if i == j then
+            STArray.writeArray arr (i,j,0) 0
+          else
+            case List.filter (\(h,_) -> h == j) (g IArray.! i) of
+              [] -> STArray.writeArray arr (i,j,0) inf
+              [(_,w')] -> STArray.writeArray arr (i,j,0) w'
+              _ -> error "Invalid case"
+      forM_ [1..vC] (\k ->
+        forM_ [1..vC] (\i ->
+          forM_ [1..vC] (\j -> do
+            case1 <- STArray.readArray arr (i,j,k-1)
+            case2a <- STArray.readArray arr (i,k,k-1)
+            case2b <- STArray.readArray arr (k,j,k-1)
+            STArray.writeArray arr (i,j,k) (min case1 (case2a + case2b))
+                        )
+                      )
+                    )
+      return arr
+
+    resultArray = IArray.array ((1,1), (vC,vC)) . map (\((i,j,_),c) -> ((i,j),c)) . filter (\((_,_,k),_) -> k == vC) . IArray.assocs $ calculated
+
+    hasNegativeCycle =
+      any (<0) . map ((IArray.!) calculated) $ diagonals
+        where
+          diagonals = map (\x -> (x,x,vC)) [1..vC]
+{-
+      where
+    memo = IArray.array ((1,1,0), (vertexCount, vertexCount, vertexCount)) [findShortestPath (i,j,k) | i <- [1..vertexCount], j <- [1..vertexCount], k <- [0..vertexCount]]
+    findShortestPath (i,j,k) =
+      if k == 0 then
+        if i == j then
+          ((i,j,k), 0)
+        else
+          case filter (\(h,_) -> h == j) ((IntMap.!) g i) of
+            [] -> ((i,j,k), 1/0)
+            [(_,w)] -> ((i,j,k), w)
+            _ -> error "Invalid graph"
+      else
+        ((i,j,k), minimum [(Array.!) memo (i,j,k-1), (Array.!) memo (i,k,k-1) + (Array.!) memo (k,j,k-1)])
+    result = Array.array ((1,1), (vertexCount, vertexCount)) . map (\((i,j,_),w) -> ((i,j),w)) . filter (\((_,_,k), _) -> k == vertexCount) . Array.assocs $ memo
+floydWarshall2 :: Array Int [(Int, Double)] -> Int -> Either String (Array (Int,Int) Double)
 floydWarshall2 g vertexCount =
   if hasNegativeCycle then
     Left "Floyd-Warshall algorithm halted: negative cycle found"
@@ -209,7 +274,7 @@ floydWarshall' g ht (i,j,k) = do
   let val = min val1 (val2a + val2b)
   HT.insert ht''' (i,j,k) val
   return (val, ht''')
-
+-}
   {-
 
 johnson :: Map.Map Int [(Int, Int)] -> Int -> Either String (Map.Map (Int,Int) Int)
